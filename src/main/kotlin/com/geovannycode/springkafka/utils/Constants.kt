dbfcdafe2e8dbb98fa32f22b715cdce6e018db97
Geovanny Mendoza <geovanny0401@gmail.com>
2023-04-25 22:31:34 -0500

    [V1-0] add config kafka and create file constant

---
+++ src/main/kotlin/com/geovannycode/springkafka/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.geovannycode.springkafka.utils
+
+const val TOPIC_NAME = "someTopicOne"
+
+const val GROUP_ID = "groupId"
---
+++ src/test/kotlin/com/geovannycode/springkafka/SpringkafkaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.geovannycode.springkafka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringkafkaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
