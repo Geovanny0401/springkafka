a47591e1196e15a1a48b916607fa2a9ea69b0df5
Geovanny Mendoza <geovanny0401@gmail.com>
2023-04-25 22:48:08 -0500

    V[1-3] update post

--- src/main/kotlin/com/geovannycode/springkafka/controller/ExampleController.kt
+++ src/main/kotlin/com/geovannycode/springkafka/controller/ExampleController.kt
@@ -2,11 +2,16 @@ package com.geovannycode.springkafka.controller
 
 import com.geovannycode.springkafka.dto.RequestBodyDto
 import com.geovannycode.springkafka.producer.ProducerString
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ExampleController(private val producerString: ProducerString) {
+    @PostMapping("/test")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     fun sendTestMessage(@RequestBody requestBody: RequestBodyDto){
         producerString.sendStringMessage(message = requestBody.message)
     }
